134bf8b3450e9aedd63868fdbb44c7e07311c2cb
atharvasaraf <atharvasaraf@yahoo.in>
2019-03-05 23:52:32 +0530

    First push

--- installation_scripts.sh
+++ installation_scripts.sh
@@ -1,2 +1,21 @@
-#!/usr/bin/bash
-sudo apt-get update
+#!/bin/bash
+sudo apt update
+sudo apt upgrade #Lol, isnt this just funny?
+#List of all the stuff I wanna get:
+#chrome
+#htop
+#sublime
+#system load indicator
+#terminator
+#vim
+#skype
+#anaconda
+
+
+# Just Dual_boot Things, use settime to correct your time immediately
+{
+        echo 'function settime {'
+        echo '	sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z";'
+        echo '	timedatectl set-local-rtc 0; '
+        echo '}'
+} >> /home/${USER}/.bashrc
