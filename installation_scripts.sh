7145ee183f141167f2f8bcce5e63e3a70025bf26
atharvasaraf <atharvasaraf@yahoo.in>
2019-03-07 00:09:07 +0530

    Added Subl, Vim, SystemLoad, Htop: Need to Organize better LMAO!

--- installation_scripts.sh
+++ installation_scripts.sh
@@ -4,10 +4,37 @@ sudo apt upgrade #Lol, isnt this just funny?
 #List of all the stuff I wanna get:
 #chrome
 #htop
+sudo apt-get update
+sudo apt-get install htop
 #sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+echo 'You just got Sublime!'
 #system load indicator
+echo 'Installing System Load Indicator!'
+sudo add-apt-repository ppa:indicator-multiload/stable-daily
+sudo apt update
+sudo apt install -y indicator-multiload
+echo 'System Load Indicator is Love!'
 #terminator
+echo 'Terminator is also Love!!'
+sudo add-apt-repository ppa:gnome-terminator		
+sudo apt-get update
+sudo apt-get install terminator
 #vim
+sudo apt remove -y vim-tiny
+sudo apt update
+sudo apt install -y vim
+{
+	echo 'syntax on'
+	echo 'set autoindent'
+	echo 'set tabstop=4'
+	echo 'set number'
+	echo 'set history=50'
+} >> /home/${USER}/.vimrc
+echo 'You just got VIM lol!'
 #skype
 #anaconda
 
