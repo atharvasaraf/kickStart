14bc21dde783054d2be61c6bfb9a21861e53e1bc
Atharva J. Saraf <atharvasaraf@yahoo.in>
2019-03-16 16:47:36 +0530

    sequencing~

--- installation_scripts.sh
+++ installation_scripts.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade #Lol, isnt this just funny?
+#Add Repos here!
+sudo apt remove -y vim-tiny
+sudo add-apt-repository ppa:gnome-terminator
+sudo add-apt-repository ppa:indicator-multiload/stable-daily
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Update
+sudo apt update
+
+# Install the damn stuff!
 #List of all the stuff I wanna get:
 #chrome
 #htop
-sudo apt-get update
 sudo apt-get install htop
 #sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
 sudo apt-get install sublime-text
 echo 'You just got Sublime!'
 #system load indicator
 echo 'Installing System Load Indicator!'
-sudo add-apt-repository ppa:indicator-multiload/stable-daily
-sudo apt update
 sudo apt install -y indicator-multiload
 echo 'System Load Indicator is Love!'
 #terminator
 echo 'Terminator is also Love!!'
-sudo add-apt-repository ppa:gnome-terminator		
-sudo apt-get update
 sudo apt-get install terminator
 #vim
-sudo apt remove -y vim-tiny
 sudo apt update
 sudo apt install -y vim
 {
